e0f623b53f4ebf0eb1a495fd004244496dc2a092
Daniel Domenech Moreno <ddm19@gcloud.ua.es>
2023-12-04 21:06:58 +0100

    Route fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import logo from './logo.svg';
 import './App.scss';
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home/Home";
-import Blogs from "./pages/NotFound/NotFound";
-import Contact from "./pages/NotFound/NotFound";
 import NoPage from "./pages/NotFound/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -13,14 +11,11 @@ function App()
   return (
     <div className="App">
       <header className="App-header">
-        <script src="https://kit.fontawesome.com/07937a987b.js" crossOrigin="anonymous"></script>
         <NavBar></NavBar>
       </header>
       <body>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
       </body>
