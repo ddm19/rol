525d35997b31d00ff6a060562b03bfdacb401869
Daniel Domenech Moreno <ddm19@gcloud.ua.es>
2023-12-08 01:23:17 +0100

    Title

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,19 @@
+import './Home.scss'
+
 const Home: React.FC = () =>
 {
     return (
-        <h1>HOME</h1>
+        <div className='titleBlock'>
+            <div className='articleBGContainer'>
+                <div className='mainArticleContainer'>
+                    <h1 className="title whiteText">Hispania</h1>
+                    <p className='normalText whiteText '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate imperdiet aliquet.
+                        Nulla laoreet vulputate libero vel malesuada. Integer non tellus diam. Pellentesque lorem mi.</p>
+                    <button className='button redText normalText'>Leer Más</button>
+                </div>
+            </div>
+
+        </div>
     )
 
 }
