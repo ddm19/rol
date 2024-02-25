4d8dadec5941ea39d1e77f077be42b8324e0da2f
Daniel Domenech Moreno <ddm19@gcloud.ua.es>
2024-02-25 21:38:27 +0100

    Phone Nav without close

--- src/components/NavBar/PhoneNavbar/PhoneNavBar.tsx
+++ src/components/NavBar/PhoneNavbar/PhoneNavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import './PhoneNavBar.scss'
 import { DiscordButton } from '../DiscordButton/DiscordButton';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,6 +6,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import './PhoneNavBar.scss'
 import { NavigationLink } from '../NavBar'
 import NavButton from "../NavButton/NavButton";
+import CloseButton from 'react-bootstrap/CloseButton';
 
 interface Props 
 {
@@ -26,16 +27,38 @@ const PhoneNavBar: React.FC<Props> = (props: Props) =>
                 <FontAwesomeIcon icon={faBars} className="menu-icon navItem" />
             </button>)
     }
+    const phoneNavBarRef = useRef<HTMLDivElement>(null);
+    useEffect(() =>
+    {
+        const handleClickOutside = (event: MouseEvent) =>
+        {
+            if (phoneNavBarRef.current && !phoneNavBarRef.current.contains(event.target as Node))
+            {
+                setPhoneMenuOpen(false);
+            }
+        };
+
+        if (isPhoneMenuOpen)
+        {
+            document.addEventListener("mousedown", handleClickOutside);
+        }
+
+        return () =>
+        {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isPhoneMenuOpen]);
     return (
         <div className='navbar'>
             {toggleButton()}
-            <nav className={`${isPhoneMenuOpen ? 'active-menu' : ''} menu`}>
+            <nav ref={phoneNavBarRef} className={`${isPhoneMenuOpen ? 'active-menu' : ''} menu`}>
                 <ul className='navContainer'>
                     <div className="mobileNav">
                         {navigationLinks.map((link: NavigationLink, index: number) =>
                         {
                             return (<NavButton link={link} toggleMenu={toggleMenu} ></NavButton>)
                         })}
+                        <CloseButton></CloseButton>
                     </div>
                 </ul>
             </nav>
