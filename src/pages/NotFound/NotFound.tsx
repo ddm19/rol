de6556e8c8e844ee9a4ccabfcb829c1aff20d95e
ddm19 <ddm19@gcloud.ua.es>
2024-03-06 23:57:29 +0100

    New img

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -3,10 +3,10 @@ import './NotFound.scss';
 
 const NotFound: React.FC = () =>
 {
-  const dragonImage = "https://cdn3.iconfinder.com/data/icons/cam-the-tigeron/100/038-confused-thinking-uncertain-question-sticker-tigeron-gragon-512.png";
+  const dragonImage = `${process.env.PUBLIC_URL}/NotFound.png`;
   return (
     <div className="not-found">
-      <img src={dragonImage} alt="404 error page image" />
+      <img src={dragonImage} alt="404 error with a little confused dragon" />
       <h1>Error 404</h1>
       <h2>Un Goblin te ha robado la página</h2>
       <p>Vuelve al inicio y trata de buscar otra!</p>
