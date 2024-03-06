6fc77d238de7fb90df7b0adf5d7955782c75714c
ddm19 <ddm19@gcloud.ua.es>
2024-03-06 21:58:06 +0100

    ProductionFixes Working

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename='/rol'>
+    <BrowserRouter basename='/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
