8f26ea5576d897391bc93c2b518da2a593d1d8b4
ddm19 <ddm19@gcloud.ua.es>
2024-04-08 18:55:14 +0200

    Article rendering

---
+++ src/components/Article/types.ts
@@ -0,0 +1,36 @@
+export interface ImportedItem 
+{
+    id: number;
+    link?: string;
+    title?: string;
+    subtitle?: string;
+    shortDesc?: string;
+    image?: string;
+}
+export interface Section
+{
+    id: number;
+    title: string;
+    subtitle?: string;
+    content: string;
+    isNumbered?: boolean;
+    subSections?: Section[];
+}
+
+export interface RelatedArticle
+{
+    title: string;
+    subtitle: string;
+    link: string;
+    image?: string;
+}
+
+export interface ArticleType
+{
+    title: string;
+    date: string;
+    content: string;
+    related: RelatedArticle[];
+    sections: Section[];
+    imports: ImportedItem[];
+}
