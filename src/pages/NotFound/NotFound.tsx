d0c4fb91a283de763007fb519f500e64749f3dff
ddm19 <ddm19@gcloud.ua.es>
2024-04-08 19:13:01 +0200

    Error component

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import './NotFound.scss';
+import Error from 'components/Error/error';
 import { Link } from 'react-router-dom';
 
 const NotFound: React.FC = () =>
 {
-  const dragonImage = `${process.env.PUBLIC_URL}/NotFound.png`;
   return (
-    <>
-      <div className='notFound'>
-        <img draggable={false} className='notFound__mainImage' src={dragonImage} alt="404 error with a little confused dragon" />
-        <div className='notFound__textContainer'>
-          <h1>Ups! No encontramos esa página</h1>
-          <h2>Un goblin te ha robado la página</h2>
-          <p>Vuelve al <Link className='link link--bold link--hoverUnderline' to='/'>Inicio</Link> y trata de buscar otra</p>
-        </div>
-      </div>
-    </>
+    <Error
+      title="Ups! No encontramos esa página"
+      subtitle="Un goblin te ha robado la página"
+      homeLink={<p>Vuelve al <Link className='link link--bold link--hoverUnderline' to='/'>Inicio</Link> y trata de buscar otra</p>}
+    />
   );
 };
 
