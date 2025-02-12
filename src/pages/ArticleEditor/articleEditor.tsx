0e6c35a805168ec0a1d116996bb5caa804a44707
Daniel Domenech Moreno <ddm19dev@gmail.com>
2025-02-12 12:34:09 +0100

    feat: customtabs

--- src/pages/ArticleEditor/articleEditor.tsx
+++ src/pages/ArticleEditor/articleEditor.tsx
@@ -1,8 +1,25 @@
+import CustomTabs, { TabItem } from "components/customTabs/customTabs";
+
 const ArticleEditor = () => {
+  const tabs: TabItem[] = [
+    {
+      title: "General",
+      content: <p>This is a placeholder for the general tab content.</p>,
+    },
+    {
+      title: "Contenido",
+      content: <p>This is a placeholder for the content tab content.</p>,
+    },
+    {
+      title: "Imagen",
+      content: <p>This is a placeholder for the image tab content.</p>,
+    },
+  ];
+
   return (
     <>
-      <h1>Test</h1>
-      <span>Test</span>
+      <h1>Crear un Nuevo Articulo</h1>
+      <CustomTabs tabs={tabs} />
     </>
   );
 };
