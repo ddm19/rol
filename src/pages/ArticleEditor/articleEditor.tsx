be31e30b9097d9bfbb6a884de05a1d695e3b8643
Daniel Domenech Moreno <ddm19dev@gmail.com>
2025-02-12 23:50:41 +0100

    generalTab

--- src/pages/ArticleEditor/articleEditor.tsx
+++ src/pages/ArticleEditor/articleEditor.tsx
@@ -1,10 +1,19 @@
 import CustomTabs, { TabItem } from "components/customTabs/customTabs";
+import { useEffect, useState } from "react";
+import GeneralTab from "./components/generalTab";
+import "./articleEditor.scss";
 
 const ArticleEditor = () => {
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+
   const tabs: TabItem[] = [
     {
       title: "General",
-      content: <p>This is a placeholder for the general tab content.</p>,
+      content: <GeneralTab formData={formData} setFormData={setFormData} />,
     },
     {
       title: "Contenido",
