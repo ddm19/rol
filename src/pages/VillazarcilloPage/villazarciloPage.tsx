b9cf610758478a34c81936da4cd21ef6211ea196
dan95 el guay <ddm19dev@gmail.com>
2025-10-15 11:50:36 +0200

    feat: backbutton

--- src/pages/VillazarcilloPage/villazarciloPage.tsx
+++ src/pages/VillazarcilloPage/villazarciloPage.tsx
@@ -1,9 +1,23 @@
-const VillazarcilloPage = () => {
-    return (
-        <div>
-            <h1>Villazarcillo</h1>
-        </div>
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Button } from "@mui/material"
+import { useEffect, useRef } from "react"
 
+const VillazarcilloPage = () => {
+    const iframeRef = useRef<HTMLIFrameElement>(null);
+
+    useEffect(() => {
+        console.log(iframeRef.current?.contentWindow);
+    }, [iframeRef]);
+
+    return (
+        <>
+            <Button onClick={() => iframeRef.current?.contentWindow?.history.back()}><FontAwesomeIcon icon={faArrowLeft} /></Button>
+            <iframe ref={iframeRef} src="https://villazarcillo.vercel.app/" style={{ width: '100%', height: '100vh', border: 'none' }} title="Villazarcillo">
+
+            </iframe>
+
+        </>
     )
 }
 export default VillazarcilloPage;
